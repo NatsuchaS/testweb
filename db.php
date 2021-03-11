3d284147a4f3c11710802136f5523d76f9535f7d
bank-anu <6306021420213@fitm.kmutnb.ac.th>
2021-03-11 14:48:04 +0700

    up

--- db.php
+++ db.php
@@ -1,3 +1,4 @@
+
 <?php
     define("host","localhost");
     define("user","root");
@@ -5,6 +6,30 @@
     define("db","bookstore");
 
     $conn = new mysqli(host,user,pass,db);
-    if($conn) die("ded");
-    else echo "connected to db"
-;?>
+    $conn->query("SET NAMES UTF8");
+    
+
+    $sql = "SELECT * FROM `book`";
+    $result = $conn->query($sql);
+    
+
+
+        echo "<table border='1'>";
+        $counter = 0;
+        while ($row = $result->fetch_assoc()) {
+            if ($counter == 0) {
+                echo "<tr>";
+                foreach ($row as $key => $value) {
+                    echo "<th>{$key}</th>";
+                }
+                echo "</tr>";
+                $counter++;
+            }
+            echo "<tr>";
+            foreach ($row as $key => $value) {
+                echo "<td>{$value}</td>";
+            }
+            
+        }
+        echo "</table>";
+        ?>
